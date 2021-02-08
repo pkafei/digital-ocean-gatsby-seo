f2a2633faed61c0ac730e33d6275d36b64bb4e49
Portia <plburton@gmail.com>
2021-02-08 16:15:53 +0100

    Added whale pic and copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Whale watching for all</h1>
+    <p>Come see extraordinary whales! 🐋</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
